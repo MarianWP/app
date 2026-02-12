7efb101a57ac440524c4d155a5ad97ee2fb4764a
Marik <78969400+MarianWP@users.noreply.github.com>
2026-02-12 02:10:54 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/app/', // ← назва твого репозиторію
   plugins: [
     vue(),
     // vueDevTools(),
